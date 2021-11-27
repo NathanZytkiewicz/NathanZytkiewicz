eda212118277c6c68d08521b91d38d6ffe0a4722
NathanZytkiewicz <94869282+NathanZytkiewicz@users.noreply.github.com>
2021-11-27 19:07:36 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import calculator_button from './calculator_button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <calculator_button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/calculator_button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function calculator_button(props) {
+
+    return(
+        <input type='button' />
+    );
+}
+
+export default calculator_button;
